cf755d2b88a961edc08c022894cee16ab8ba4978
vnddn <vnddnjs00@naver.com>
2021-12-28 04:19:45 +0900

    컴포넌트 스캔

--- 02_Spring_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 02_Spring_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,16 @@
 package hello.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    //여기에다가 멤버리파지토리 타입에 맞는 애를 찾아서 의존관계를 주입한다 (오토와이어드)
+    @Autowired // ac.getBean(MemberRepository.class) 이가 자동으로 들어간다고 생각하면됨
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
