dc20320715759dbede2ccbbad88c0251f82cb769
vnddn <vnddnjs00@naver.com>
2021-12-30 05:04:11 +0900

    애노테이션 직접 만들기, 롬복과 최신 트랜드

--- 02_Spring_basic/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ 02_Spring_basic/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 @Component
+//@Qualifier("mainDiscountPolicy") //문자기 때문에 오류발생해도 모른다
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent=10;
