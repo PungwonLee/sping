624572bf7236a0af71c9195486b78a351f0b45f2
vnddn <vnddnjs00@naver.com>
2022-01-16 16:39:16 +0900

    request 정보 헤더 등.

--- 04_SpringMVC1/src/main/java/hello/servlet/ServletApplication.java
+++ 04_SpringMVC1/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //내패키지를 포함해서 서블릿을 찾아서 자동으로 실행 (서블릿 자동 등록)
 @SpringBootApplication
 public class ServletApplication {
 
