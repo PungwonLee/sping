931231f6485cd6a774cac606301586bb2316dc70
vnddn <vnddnjs00@naver.com>
2022-01-17 20:07:57 +0900

    한수

---
+++ sds/src/DAY01/P1065한수/Main.java
@@ -0,0 +1,66 @@
+package DAY01.P1065한수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
+        int cnt = 99;
+        if (N < 100) cnt = N;
+        for (int i = 100; i <= N; i++) {
+            int thr = i / 100;
+            int two = (i / 10) % 10;
+            int one = i % 10;
+            if (thr - two == two - one) cnt += 1;
+        }
+
+        System.out.println(cnt);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
