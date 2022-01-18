740c42dc31869fe99c024983f06507fb84688b44
vnddn <vnddnjs00@naver.com>
2022-01-18 13:56:20 +0900

    피보나치

---
+++ sds/src/DAY02/P2748피보나치/Main.java
@@ -0,0 +1,62 @@
+package DAY02.P2748피보나치;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd= new FastReader();
+        int N=rd.nextInt();
+        long []arr= new long[100];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i<100;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        System.out.println(arr[N-1]);
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
