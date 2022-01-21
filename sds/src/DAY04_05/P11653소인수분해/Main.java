25f614df5efc33f45a2618d9de36ff8322f2f96a
vnddn <vnddnjs00@naver.com>
2022-01-21 13:06:20 +0900

    소인수분해

---
+++ sds/src/DAY04_05/P11653소인수분해/Main.java
@@ -0,0 +1,65 @@
+package DAY04_05.P11653소인수분해;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N=rd.nextInt();
+        int i=2;
+        while(N>1){
+            if(N%i==0){
+                N/=i;
+                System.out.println(i);
+            }else{
+                i++;
+            }
+
+        }
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
