7ce020695bf8808eeb3a63705d847953e690e088
vnddn <vnddnjs00@naver.com>
2022-01-24 02:30:48 +0900

    중복X 사전순 출력

---
+++ sds/src/DAY05/P15663N과M_9/Main.java
@@ -0,0 +1,92 @@
+package DAY05.P15663N과M_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int arr[], select[], check[];
+    static HashSet<String> hs= new LinkedHashSet<>();
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        N = rd.nextInt();
+        M = rd.nextInt();
+        arr = new int[N];
+        select = new int[M];
+        check = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = rd.nextInt();
+        }
+        Arrays.sort(arr);
+        func(0);
+        for (String h : hs) {
+            System.out.println(h);
+        }
+    }
+
+    private static void func(int v) {
+        if (v == M) {
+            StringBuilder sb2= new StringBuilder();
+            for (int i : select) {
+                sb2.append(i+" ");
+            }
+            hs.add(sb2.toString());
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (check[i] == 0) {
+                    check[i] = 1;
+                    select[v] = arr[i];
+                    func(v + 1);
+                    check[i] = 0;
+                }
+            }
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
