8c24a5e7f7634167a96dd632e0df70b57c699e8f
vnddn <vnddnjs00@naver.com>
2022-01-31 01:31:10 +0900

    위상정렬

---
+++ sds/src/DAY06/P2458키순서/Main.java
@@ -0,0 +1,97 @@
+package DAY06.P2458키순서;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
+        int M = rd.nextInt();
+
+        long arr[][] = new long[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= N; j++) {
+                arr[i][j] = INF;
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        while (M-- != 0) {
+            int u = rd.nextInt();
+            int v = rd.nextInt();
+            arr[u][v] = 1;
+
+        }
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= N; j++) {
+                if (arr[i][j] == INF && arr[j][i] == INF) {
+                    cnt++;
+                }
+            }
+            if (cnt == 0) {
+                res += 1;
+            }
+        }
+        System.out.print(res);
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
