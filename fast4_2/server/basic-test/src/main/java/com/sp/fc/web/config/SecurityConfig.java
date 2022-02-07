909ca0ffe50825c6632e9dd67ddcaab9787ddebf
vnddn <vnddnjs00@naver.com>
2022-02-07 16:24:43 +0900

    초기

--- fast4_2/server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ fast4_2/server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sp.fc.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@Order(1) // 2개이상일때 먼저 필터 실행해야하는 경우가 있을수가있기때문에 Order로 우선순위를 정한다.
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)//prePost로 권한을 체크하겠다
 
