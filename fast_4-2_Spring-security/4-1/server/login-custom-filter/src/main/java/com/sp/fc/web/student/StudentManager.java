e34427418d54b64f0563c57ed2733fcf0345f70d
vnddn <vnddnjs00@naver.com>
2022-02-10 21:35:14 +0900

    student통행증

---
+++ fast_4-2_Spring-security/4-1/server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
@@ -0,0 +1,51 @@
+package com.sp.fc.web.student;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.sql.Struct;
+import java.util.HashMap;
+import java.util.Set;
+
+//통행증 발급
+@Component
+public class StudentManager implements AuthenticationProvider , InitializingBean {
+
+    private HashMap<String, Student> studentDB= new HashMap<>();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        if(studentDB.containsKey(token.getName())){
+            Student student= studentDB.get(token.getName());
+            return StudentAuthenticationToken.builder()
+                    .principal(student)
+                    .details(student.getUsername())
+                    .authenticated(true)
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication == UsernamePasswordAuthenticationToken.class;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Set.of(
+                new Student("hong","홍길동",Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("kang","강아지",Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("hoo","랑이",Set.of(new SimpleGrantedAuthority("ROLE_USER")))
+        ).forEach(s->
+                studentDB.put(s.getId(),s));
+    }
+}
