ad339e6e3bc409752c0fa306bce201090b099666
vnddn <vnddnjs00@naver.com>
2022-02-11 17:15:29 +0900

    인증제공자 커스텀

--- fast_4-2_Spring-security/4-1/server/login-custom-filter/src/main/java/com/sp/fc/web/config/CustomLoginFilter.java
+++ fast_4-2_Spring-security/4-1/server/login-custom-filter/src/main/java/com/sp/fc/web/config/CustomLoginFilter.java
@@ -1,6 +1,8 @@
 package com.sp.fc.web.config;
 
 
+import com.sp.fc.web.student.StudentAuthenticationToken;
+import com.sp.fc.web.teacher.TeacherAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,9 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
-    public CustomLoginFilter(AuthenticationManager authenticationManager){
+    public CustomLoginFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String username = obtainUsername(request);
@@ -21,9 +24,23 @@ public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
         username = username.trim();
         String password = obtainPassword(request);
         password = (password != null) ? password : "";
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+        String type = request.getParameter("type");
 
-        return super.attemptAuthentication(request, response);
+        if (type == null || !type.equals("teacher")) {
+            //student
+            StudentAuthenticationToken token = StudentAuthenticationToken.builder()
+                    .credentials(username).build();
+            return this.getAuthenticationManager().authenticate(token);
+
+        } else {
+            //teacher
+            TeacherAuthenticationToken token = TeacherAuthenticationToken.builder()
+                    .credentials(username).build();
+
+            return this.getAuthenticationManager().authenticate(token);
+
+        }
+
         //보통 attemptAuthentication 이 토큰을 만드는 곳
     }
 }
