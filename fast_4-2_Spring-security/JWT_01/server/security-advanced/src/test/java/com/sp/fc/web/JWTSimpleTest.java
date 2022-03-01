55f3ab8750abaefc5e3a5e7df46f36b9b1327073
vnddn <vnddnjs00@naver.com>
2022-03-01 17:44:59 +0900

    JWT 테스트 헤더.바디.헤더+바디 해쉬

--- fast_4-2_Spring-security/JWT_01/server/security-advanced/src/test/java/com/sp/fc/web/JWTSimpleTest.java
+++ fast_4-2_Spring-security/JWT_01/server/security-advanced/src/test/java/com/sp/fc/web/JWTSimpleTest.java
@@ -1,15 +1,31 @@
 package com.sp.fc.web;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+import java.util.Map;
+
 public class JWTSimpleTest {
 
+    private void printToken(String token){
+        String[] tokens = token.split("\\.");
+        System.out.println("header: "+new String(Base64.getDecoder().decode(tokens[0])));
+        System.out.println("body: "+new String(Base64.getDecoder().decode(tokens[1])));
+    }
 
     @DisplayName("1. jjwt 를 이용한 토큰 테스트")
     @Test
-    void test_1(){
-
+    void test_1() {
+        String okta_token = Jwts.builder().addClaims(
+                        Map.of("name", "pungwon", "price", 3000)
+                ).signWith(SignatureAlgorithm.HS256, "pungwon")
+                .compact();
+        System.out.println("okta_token = " + okta_token);
+        // "헤더"."바디"."헤더와바디 해쉬화 으로 구성됨
+        printToken(okta_token);
     }
 
 
@@ -18,7 +34,6 @@ public class JWTSimpleTest {
     void test_2() {
 
     }
-
 
 
 }
