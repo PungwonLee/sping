10e86ea83683be8974014e24ead5bc8bfd8f2550
vnddn <vnddnjs00@naver.com>
2022-03-05 16:55:26 +0900

    JWT 만료시간, 토큰테스트

--- fast_4-2_Spring-security/JWT_01/server/security-advanced/src/test/java/com/sp/fc/web/JWTSimpleTest.java
+++ fast_4-2_Spring-security/JWT_01/server/security-advanced/src/test/java/com/sp/fc/web/JWTSimpleTest.java
@@ -1,19 +1,27 @@
 package com.sp.fc.web;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.Base64;
+import java.util.Date;
 import java.util.Map;
 
 public class JWTSimpleTest {
 
-    private void printToken(String token){
+    private void printToken(String token) {
         String[] tokens = token.split("\\.");
-        System.out.println("header: "+new String(Base64.getDecoder().decode(tokens[0])));
-        System.out.println("body: "+new String(Base64.getDecoder().decode(tokens[1])));
+        System.out.println("header: " + new String(Base64.getDecoder().decode(tokens[0])));
+        System.out.println("body: " + new String(Base64.getDecoder().decode(tokens[1])));
     }
 
     @DisplayName("1. jjwt 를 이용한 토큰 테스트")
@@ -26,6 +34,9 @@ public class JWTSimpleTest {
         System.out.println("okta_token = " + okta_token);
         // "헤더"."바디"."헤더와바디 해쉬화 으로 구성됨
         printToken(okta_token);
+
+        Jws<Claims> tokenInfo = Jwts.parser().setSigningKey("pungwon").parseClaimsJws(okta_token);
+        System.out.println("tokenInfo = " + tokenInfo);
     }
 
 
@@ -33,7 +44,38 @@ public class JWTSimpleTest {
     @Test
     void test_2() {
 
+        byte[] SEC_KEY = DatatypeConverter.parseBase64Binary("pungwon");
+
+        String oauth0_token = JWT.create().withClaim("name", "pungwon").withClaim("price", 3000)
+                .sign(Algorithm.HMAC256(SEC_KEY));
+        System.out.println("oauth0_token = " + oauth0_token);
+        printToken(oauth0_token);
+
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SEC_KEY)).build().verify(oauth0_token);
+        System.out.println("verify = " + verify);
+
+        Jws<Claims> tokenInfo = Jwts.parser().setSigningKey(SEC_KEY).parseClaimsJws(oauth0_token);
+        System.out.println("tokenInfo = " + tokenInfo);
+
     }
 
+    @DisplayName("3. 만료 시간 테스트")
+    @Test
+    void test_3() {
+        Algorithm AL = Algorithm.HMAC256("pungwon");
+        String token = JWT.create().withSubject("a1234")
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000))
+                .withNotBefore(new Date(System.currentTimeMillis() + 3000))
+                .sign(AL);
+        try {
+            DecodedJWT verify = JWT.require(AL).build().verify(token);
+            System.out.println("verify.getClaims() = " + verify.getClaims());
+
+        } catch (Exception ex) {
+            System.out.println("유효하지 않은 토큰입니다...");
+            DecodedJWT decode = JWT.decode(token);
+            System.out.println("decode.getClaims() = " + decode.getClaims());
+        }
+    }
 
 }
